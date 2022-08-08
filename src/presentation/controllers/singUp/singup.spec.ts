2c55204438090db6db6292fb0b9a651526a9f2d3
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-08 16:52:07 -0300

    test: ensure return 200 if was sended valid data

--- src/presentation/controllers/singUp/singup.spec.ts
+++ src/presentation/controllers/singUp/singup.spec.ts
@@ -70,18 +70,24 @@ describe('SingUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissParamError('email'))
   })
 
-  test('Should return 200 if name and email is provided', () => {
+  test('Should return 200 if valid data is provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email: 'any_mail',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
+        name: 'valid_name',
+        email: 'valid@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'valid_password'
+    })
   })
 
   test('Should return 400 if password no provided', () => {
