b5ef4743cbaacc1cf54bd51cdfd0560d75749c2c
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-09 14:23:09 -0300

    test: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from './helpers/mongo-helper'
+import { AccountMongoReposiroty } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoReposiroty()
+    const account = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email')
+    expect(account.password).toBe('valid_password')
+  })
+})
