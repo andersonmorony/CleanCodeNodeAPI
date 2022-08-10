61beac9378f90ec39e6089bdad69ebafe37008f7
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-10 11:00:16 -0300

    feat: ensure mongodb reconnect ig client is down

--- src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/account-repository/helpers/mongo-helper.ts
@@ -2,13 +2,19 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: MongoClient,
+  uri: '' as string,
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri)
+    this.uri = uri
   },
   async disconnect (): Promise<void> {
     this.client.close()
+    this.client = null
   },
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.db()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   }
 }
