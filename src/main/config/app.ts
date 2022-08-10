48ecf260e724d37eebd9b23a7b0ee35681d3dac9
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-10 07:43:55 -0300

    feat: ensure singup route return 200 on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddleware from './middlawares'
+import setupRoute from './routes'
 
 const app = express()
 setupMiddleware(app)
+setupRoute(app)
 export default app
