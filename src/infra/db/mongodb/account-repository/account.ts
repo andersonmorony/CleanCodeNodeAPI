30e0aebf08f413e95b3aac093f0780def022756a
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-10 06:32:31 -0300

    feat: add map to map model

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,18 +2,14 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecase/add-account'
 import { MongoHelper } from './helpers/mongo-helper'
+import { map } from './account-mapper'
 
 export class AccountMongoReposiroty implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    const account = (await accountCollection.findOne<AccountModel>({ _id: result.insertedId }) as any)
-    console.log(account)
-    return {
-      id: account._id,
-      name: account.name,
-      email: account.email,
-      password: account.password
-    }
+    const account = await accountCollection.findOne<AccountModel>({ _id: result.insertedId })
+
+    return map(account)
   }
 }
