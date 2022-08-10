bfea202ab6de6661ab94775e913aa396c3c5312d
Andersonmorony <andersonmoroni92@gmail.com>
2022-08-10 11:00:38 -0300

    test: ensure mongodb reconnect ig client is down

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -12,7 +12,7 @@ describe('Singup Router', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
